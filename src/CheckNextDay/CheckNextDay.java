108be77a1a2d329c5d31c8505ede7ccd7faaba30
Your Name <you@example.com>
2021-10-18 10:29:28 +0700

    test the thui

---
+++ src/CheckNextDay/CheckNextDay.java
@@ -0,0 +1,10 @@
+package CheckNextDay;
+
+public class CheckNextDay {
+    public static String NextDay(int day,int month,int year){
+        day++;
+        return day+" "+month+" "+year;
+    }
+
+
+}
